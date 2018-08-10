a86b4a9fad258cf851816c6654cc5b39d58538c9
Forrest Edwards <froohoo@gmail.com>
2018-08-09 22:58:49 -0500

    fix: removed junk from beginning of file

--- ShellScripts/test_slam.sh
+++ ShellScripts/test_slam.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-source /home/workspace/catkin_ws/devel/setup.garbage
-export TURTLEBOT_GAZEBO_WORLD_FILE="/boguspath/bogusfile.world"
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 3
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
